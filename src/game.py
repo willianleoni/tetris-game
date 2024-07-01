7f674bca5f63ad37c1ddb7757417cf1ba4f33756
Willian Dorneles Leoni <willianleoni@gmail.com>
2024-07-01 16:34:12 -0300

    add blocks and positions logic

--- src/game.py
+++ src/game.py
@@ -1,5 +1,6 @@
 from pygame.sprite import Group
 from settings import *
+from random import choice
 
 class Game():
     def __init__(self):
@@ -16,7 +17,7 @@ class Game():
         self.line_surface.set_colorkey((0,255,0))
         self.line_surface.set_alpha(120)
 
-        self.block = Block(self.sprites)
+        self.tetromino = Tetromino(choice(list(TETROMINOS.keys())), self.sprites)
 
     def draw_grid(self):
 
@@ -40,9 +41,25 @@ class Game():
         self.display_surface.blit(self.surface, (PADDING,PADDING))
         pygame.draw.rect(self.display_surface, LINE_COLOR, self.rect, 2, 2)
 
+class Tetromino:
+    def __init__(self, shape, group):
+        # setup
+        self.block_positions = TETROMINOS[shape]['shape']
+        self.color = TETROMINOS[shape]['color']
+
+        # create blocks
+        self.blocks = [Block(group, pos, self.color) for pos in self.block_positions]
+
 class Block(pygame.sprite.Sprite):
-    def __init__(self, group):
+    def __init__(self, group, pos, color):
+        
+        # general
         super().__init__(group)
         self.image = pygame.Surface((CELL_SIZE,CELL_SIZE))
-        self.image.fill('red')
-        self.rect = self.image.get_rect(topleft = (0,0))
+        self.image.fill(color)
+
+        # position
+        self.pos = pygame.Vector2(pos) + BLOCK_OFFSET
+        x = self.pos.x * CELL_SIZE
+        y = self.pos.y * CELL_SIZE
+        self.rect = self.image.get_rect(topleft = (x,y))
--- src/settings.py
+++ src/settings.py
@@ -16,6 +16,12 @@ PADDING = 20
 WINDOW_WIDTH = GAME_WIDTH + SIDEBAR_WIDTH + PADDING * 3
 WINDOW_HEIGHT =  GAME_HEIGHT + PADDING * 2
 
+# game behavior
+UPDATE_START_SPEED = 800
+MOVE_WAIT_TIME = 200
+ROTATE_WAIT_TIME = 200
+BLOCK_OFFSET = pygame.Vector2(COLUMNS // 2, 3)
+
 # colors
 YELLOW = "#f1e60d"
 RED = "#e51b20"
@@ -26,3 +32,15 @@ CYAN = "#6cc6d9"
 ORANGE = "#f07e13"
 GRAY = "#1C1C1C"
 LINE_COLOR = "#FFFFFF"
+
+TETROMINOS = {
+    'T': {'shape': [(0,0),(-1,0),(1,0),(0,-1)], 'color' : PURPLE},
+    'O': {'shape': [(0,0),(0,-1),(1,0),(1,-1)], 'color' : YELLOW},
+    'J': {'shape': [(0,0),(0,-1),(0,1),(-1,1)], 'color' : BLUE},
+    'L': {'shape': [(0,0),(0,-1),(0,1),(1,1)], 'color' : ORANGE},
+    'I': {'shape': [(0,0),(0,-1),(0,-2),(0,1)], 'color' : CYAN},
+    'S': {'shape': [(0,0),(-1,0),(0,-1),(1,-1)], 'color' : GREEN},
+    'Z': {'shape': [(0,0),(1,0),(0,-1),(-1,-1)], 'color' : RED}
+}
+
+SCORE_DATA = {1: 40, 2: 100, 3: 300, 4: 1200}
