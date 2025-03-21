a4b1a8a94073f233fead2902a766f4f49321d458
willian <willian-leoni-isi@senairs.org.br>
2025-03-20 23:56:35 -0300

    feat: add preview, movements, scores, timer and assets

--- src/main.py
+++ src/main.py
@@ -1,42 +1,65 @@
 from settings import *
 from sys import exit
+from os.path import join
 
 # components
 from game import Game
 from score import Score
 from preview import Preview
 
+from random import choice
+
 class Main:
-    def __init__(self):
+	def __init__(self):
 
-        # general
-        pygame.init()
-        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-        self.clock = pygame.time.Clock()
-        pygame.display.set_caption('Tetris')
+		# general 
+		pygame.init()
+		self.display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+		self.clock = pygame.time.Clock()
+		pygame.display.set_caption('Tetris')
 
-        # components
-        self.game = Game()
-        self.score = Score()
-        self.preview = Preview()
+		# shapes
+		self.next_shapes = [choice(list(TETROMINOS.keys())) for shape in range(3)]
 
-    def run(self):
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    exit()
+		# components
+		self.game = Game(self.get_next_shape, self.update_score)
+		self.score = Score()
+		self.preview = Preview()
 
-            # display
-            self.display_surface.fill(GRAY)
-            self.game.run()
-            self.score.run()
-            self.preview.run()
+		# audio 
+		self.music = pygame.mixer.Sound(join('..','sound','music.wav'))
+		self.music.set_volume(0.07)
+		self.music.play(-1)
 
-            # updating game
-            pygame.display.update()
-            self.clock.tick()
+	def update_score(self, lines, score, level):
+		self.score.lines = lines
+		self.score.score = score
+		self.score.level = level
+
+	def get_next_shape(self):
+		next_shape = self.next_shapes.pop(0)
+		self.next_shapes.append(choice(list(TETROMINOS.keys())))
+		return next_shape
+
+	def run(self):
+		while True:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.quit()
+					exit()
+
+			# display 
+			self.display_surface.fill(GRAY)
+			
+			# components
+			self.game.run()
+			self.score.run()
+			self.preview.run(self.next_shapes)
+
+			# updating the game
+			pygame.display.update()
+			self.clock.tick()
 
 if __name__ == '__main__':
-    main = Main()
-    main.run()
+	main = Main()
+	main.run()
